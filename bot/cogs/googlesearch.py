6d193e0b21609250eb93cadac321b82d071de591
Ashwani Gupta <ashwanigupta125@gmail.com>
2020-04-10 17:57:14 +0530

    Add embed formatted search results

--- bot/cogs/googlesearch.py
+++ bot/cogs/googlesearch.py
@@ -1,3 +1,4 @@
+from discord import Embed, Colour
 from discord.ext import commands
 from googlesearch import search
 
@@ -6,28 +7,32 @@ class GoogleSearch(commands.Cog, name='Search on Google'):
         self.bot = bot
 
     @commands.command(name='google')
-    async def google_search(self, ctx, query: str):
+    async def google_search(self, ctx, query: str, limit: int):
         """ Finds search results from Google search 
         Usage: !google <query>
         
         Optionally, you can ask for more than 5 results by providing limit
         Usage: !google <query> <limit>
 
+        Note: Please use quotes(") if you are asking with multiple words. 
+        I am dumb to understand without quotes :(
+
         Examples: 
             !google hello
             !google hello 10
+            !google "hello world" 3
         """
-        if query.split(' ')[-1].isnumeric():
-            query, limit = query.rsplit(' ', maxsplit=1)
-        else:
-            limit = 5
-            
+
         await ctx.send(f'{ctx.author.mention} Hold on! Fetching search results for you... :clock:')
         async with ctx.channel.typing():
             results = search(query, stop=limit)
-
-            await ctx.send('\n'.join(results))
-
+            numbered_results = [f'{index+1}. {url}' for index, url in enumerate(results)]
+            embed = Embed(
+                title=f'Search results for `{query}`',
+                description='\n'.join(numbered_results),
+                colour=Colour.blue()
+            )
+            await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(GoogleSearch(bot))
